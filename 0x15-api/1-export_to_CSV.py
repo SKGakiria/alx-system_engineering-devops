e1292db91cdf3f1a8b7f9c6c3a2f4bfd2be3fa7e
SKGakiria <swgakiria@gmail.com>
2023-07-24 09:12:20 -0700

    Added Task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,16 +2,19 @@
 """Employee TODO list progress information"""
 import requests
 import sys
+import csv
 
 
 if __name__ == "__main__":
     usr_id = sys.argv[1]
     b_url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(b_url + "users/{}".format(usr_id)).json()
+    u_name = user.get("username")
     todos = requests.get(b_url + "todos",
                          params={"userId": usr_id}).json()
 
-    done_tasks = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(done_tasks), len(todos)))
-    [print("\t {}".format(d)) for d in done_tasks]
+    with open("{}.csv".format(usr_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [usr_id, u_name, t.get("completed"), t.get("title")]
+         ) for t in todos]
