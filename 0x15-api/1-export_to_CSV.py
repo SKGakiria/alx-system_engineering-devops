3d067b41cbc81f9145bc2412fa4ebb7a37d1ae20
SKGakiria <swgakiria@gmail.com>
2023-07-24 09:18:40 -0700

    Added Task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     todos = requests.get(b_url + "todos",
                          params={"userId": usr_id}).json()
 
-    with open("{}.csv".format(usr_id), "w", newline="") as csvfile:
+    with open("{}.csv".format(usr_id), "w") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow(
             [usr_id, u_name, t.get("completed"), t.get("title")]
