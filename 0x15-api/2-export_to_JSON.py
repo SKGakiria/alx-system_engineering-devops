546e5235b779d7ecfea8dcbe605344abf4177c3b
SKGakiria <swgakiria@gmail.com>
2023-07-24 09:30:15 -0700

    Added file Task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Employee TODO list progress information"""
+import requests
+import json
+import sys
+
+
+if __name__ == "__main__":
+    usr_id = sys.argv[1]
+    b_url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(b_url + "users/{}".format(usr_id)).json()
+    u_name = user.get("username")
+    todos = requests.get(b_url + "todos",
+                         params={"userId": usr_id}).json()
+
+    with open("{}.json".format(usr_id), "w") as jsonfile:
+        json.dump({usr_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u_name
+            } for t in todos]}, jsonfile)
