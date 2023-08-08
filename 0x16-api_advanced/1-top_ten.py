d140c0e26eeba80b0dd940d00b285ec0fcd62712
SKGakiria <swgakiria@gmail.com>
2023-08-08 15:15:19 -0700

    Updated Task 2 and added Task 3

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,15 +6,15 @@ import requests
 def top_ten(subreddit):
     """Function queries and prints the titles of the first 10 hot posts
     listed for a given subreddit"""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    URL = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
                "User-Agent": "Ubuntu.20.04:alx.advanced.api:v1.0 (by /u/sk001)"
     }
     params = {"limit": 10}
-    response = requests.get(url, headers=headers, params=params,
+    response = requests.get(URL, headers=headers, params=params,
                             allow_redirects=False)
     if response.status_code == 404:
         print("None")
         return
     results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    [print(k.get("data").get("title")) for k in results.get("children")]
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,7 +3,31 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=None, count=0):
     """Function queries and returns a list containing the titles of all hot
     articles for a given subreddit recursively"""
+    URL = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {
+               "User-Agent": "Ubuntu.20.04:alx.advanced.api:v1.0 (by /u/sk001)"
+    }
 
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 50
+    }
+
+    response = requests.get(URL, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        return None
+
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for k in results.get("children"):
+        hot_list.append(k.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
