f78e058bcd9087fc1482d8feffc3c783b8bec8c8
SKGakiria <swgakiria@gmail.com>
2023-08-15 10:13:24 +0000

    Added Puppet manifest for Task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# Test how well the web server setup featuring Nginx is doing under pressure
+
+exec {'24: Too many open files':
+  command => 'sed -i "s/15/5000/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+exec {'Restart nginx':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
